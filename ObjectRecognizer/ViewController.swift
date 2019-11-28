e7931296d8a968977496b1b15cd5b62fe408bb66
Mihnea Stefan <mihnea.stefan@protonmail.com>
2019-11-28 15:21:04 +0200

    Initial Commit

---
+++ ObjectRecognizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObjectRecognizer
+//
+//  Created by Mihnea Stefan on 28/11/2019.
+//  Copyright © 2019 Mihnea Stefan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
